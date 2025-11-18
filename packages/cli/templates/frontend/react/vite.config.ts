8b3caed4d28c838b5857e150c1947ac4e877cceb
0zd0 <67220210+0zd0@users.noreply.github.com>
2025-11-18 05:33:47 +0300

    feat(cli): npm packages

--- packages/cli/templates/frontend/react/vite.config.ts
+++ packages/cli/templates/frontend/react/vite.config.ts
@@ -1,45 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { join } from 'node:path'
-import externalGlobals, { type ModuleNameMap } from 'rollup-plugin-external-globals'
-
-const externalGlobalsOnlyProd: ModuleNameMap = {
-    react: 'window.React',
-}
+import lunaPressWordPress from '@lunapress/rollup-plugin-wp'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
     return {
-        plugins: [
-            react(),
-            externalGlobals({
-                jquery: 'window.jQuery',
-                'lodash-es': 'window.lodash',
-                lodash: 'window.lodash',
-                moment: 'window.moment',
-                'react-dom': 'window.ReactDOM',
-                react: 'window.React',
-
-                '@wordpress/hooks': 'wp.hooks',
-                '@wordpress/element': 'wp.element',
-                '@wordpress/blocks': 'wp.blocks',
-                '@wordpress/data': 'wp.data',
-                '@wordpress/api-fetch': 'wp.apiFetch',
-                '@wordpress/components': 'wp.components',
-                '@wordpress/warning': 'wp.warning',
-                '@wordpress/compose': 'wp.compose',
-                '@wordpress/i18n': 'wp.i18n',
-
-                '@carbon-fields/core': 'cf.core',
-                '@carbon-fields/hooks': 'cf.hooks',
-                '@carbon-fields/@wordpress/element': "cf.vendor['@wordpress/element']",
-
-                ...(mode !== 'development' ? externalGlobalsOnlyProd : {}),
-            }),
-        ],
-        server: {
-            cors: true,
-        },
+        plugins: [react(), lunaPressWordPress()],
         build: {
             manifest: true,
             emptyOutDir: true,
@@ -66,9 +33,6 @@ export default defineConfig(({ mode }) => {
                     replacement: join(__dirname, 'src/modules'),
                 },
             ],
-        },
-        optimizeDeps: {
-            exclude: ['react', 'react-dom'],
         },
     }
 })
